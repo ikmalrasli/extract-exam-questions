b9ce3367a720b01909c6d3653c139eaab63e386c
eemauuu <ikmalrasli@gmail.com>
2025-04-04 16:52:08 +0800

    diagram component now displays image via url key

--- client/src/components/jsoneditor/DiagramContent.vue
+++ client/src/components/jsoneditor/DiagramContent.vue
@@ -5,11 +5,15 @@ const props = defineProps({
     required: true
   }
 })
+
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center">
-    <div class="block flex max-w-lg w-full h-[200px] border border-black bg-gray-300 text-center items-center justify-center">
+    <img v-if="content.url" :src="content.url" alt="Diagram Image" class="mt-2 max-w-3/4 h-auto" />
+    <div v-else
+      class="block flex flex-col max-w-lg w-full h-[200px] border border-black bg-gray-300 text-center items-center justify-center">
+      <span class="block text-xl font-bold opacity-20">-Diagram not extracted-</span>
       <span class="block text-xl font-bold opacity-20">Page {{ content.page }}</span>
     </div>
     <span class="block">Rajah {{ content.number }}</span>
